2876ca4d2da6528d86513584fedac446b07acf29
siddhant2397 <67710961+siddhant2397@users.noreply.github.com>
2025-08-28 02:40:45 +0530

    Update taskmanager.py

--- taskmanager.py
+++ taskmanager.py
@@ -73,8 +73,8 @@ st.markdown("""
 
 # Login Interface
 st.sidebar.header("Login")
-username = st.sidebar.text_input("Username", key = "login_username")
-password = st.sidebar.text_input("Password", type="password", key = "login_password")
+username = st.sidebar.text_input("Username", key = "login_username", value=st.session_state.get("login_username", ""))
+password = st.sidebar.text_input("Password", type="password", key = "login_password", value=st.session_state.get("login_password", ""))
 login_btn = st.sidebar.button("Login")
 
 if 'logged_in' not in st.session_state:
@@ -95,10 +95,8 @@ if st.session_state.get("logged_in"):
         st.session_state.logged_in = False
         st.session_state.username = None
         st.session_state.role = None
-        if "login_username" in st.session_state:
-            del st.session_state["login_username"]
-        if "login_password" in st.session_state:
-            del st.session_state["login_password"]
+        st.session_state["login_username"] = ""
+        st.session_state["login_password"] = ""
         st.rerun()  # Refresh page to show login screen
 
 
